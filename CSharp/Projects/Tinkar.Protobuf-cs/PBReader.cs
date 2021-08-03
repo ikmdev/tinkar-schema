0556475c13f8beeea70230978e4c6ddbd3057186
Kurt Allen <KurtA@Gjoll.com>
2021-08-03 17:40:17 -0500

    PB Reader/Writer now zips data

--- CSharp/Projects/Tinkar.Protobuf-cs/PBReader.cs
+++ CSharp/Projects/Tinkar.Protobuf-cs/PBReader.cs
@@ -1,21 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Text;
 using Tinkar.ProtoBuf.CS;
 
 namespace Tinkar.ProtoBuf.CS
 {
-    public class PBReader
+    /// <summary>
+    /// Read protobuf tinkar items from protobuf zip input stream.
+    /// </summary>
+    public class PBReader : IDisposable
     {
-        Stream inputStream;
+        Stream zipFile;
+        ZipArchive zipArchive;
+        ZipArchiveEntry zipEntry;
+        Stream stream;
+
         public PBReader(Stream inputStream)
         {
-            this.inputStream = inputStream;
+            this.zipFile = inputStream;
+            this.zipArchive = new ZipArchive(this.zipFile);
+            this.zipEntry = this.zipArchive.GetEntry("export.pb");
+            this.stream = this.zipEntry.Open();
         }
 
+        public void Dispose()
+        {
+            if (this.zipArchive != null)
+            {
+                this.zipArchive.Dispose();
+                this.zipArchive = null;
+                this.zipFile = null;
+                this.zipEntry = null;
+                this.stream = null;
+            }
+        }
+
+        /// <summary>
+        /// Read single protobuf message.
+        /// </summary>
         public PBTinkarMsg Read() =>
-            PBTinkarMsg.Parser.ParseDelimitedFrom(inputStream);
+            PBTinkarMsg.Parser.ParseDelimitedFrom(this.stream);
     }
 
 }
--- CSharp/Projects/Tinkar.Protobuf-cs/PBWriter.cs
+++ CSharp/Projects/Tinkar.Protobuf-cs/PBWriter.cs
@@ -2,19 +2,47 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Text;
 
 namespace Tinkar.ProtoBuf.CS
 {
-    public class PBWriter
+    /// <summary>
+    /// Write Protobuf Tinkar items to zip output file.
+    /// </summary>
+    public class PBWriter : IDisposable
     {
-        Stream outputStream;
-        public PBWriter(Stream outputStream)
+        Stream zipFile;
+        ZipArchive zipArchive;
+        ZipArchiveEntry zipEntry;
+        Stream stream;
+
+        public PBWriter(Stream zipFile)
         {
-            this.outputStream = outputStream;
+            this.zipFile = zipFile;
+            this.zipArchive = new ZipArchive(this.zipFile, ZipArchiveMode.Create);
+            this.zipEntry = this.zipArchive.CreateEntry("export.pb");
+            this.stream = this.zipEntry.Open();
+
         }
 
+        public void Dispose()
+        {
+            if (this.zipArchive != null)
+            {
+                this.zipArchive.Dispose();
+                this.zipArchive = null;
+                this.zipFile = null;
+                this.zipEntry = null;
+                this.stream = null;
+            }
+        }
+
+        /// <summary>
+        /// Write single protobuf message.
+        /// </summary>
+        /// <param name="msg"></param>
         public void Write(PBTinkarMsg msg) =>
-            msg.WriteDelimitedTo(outputStream);
+            msg.WriteDelimitedTo(stream);
     }
 }
